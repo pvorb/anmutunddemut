607742ca3ead62af4ff645cc687ee8b15a7af75a
Webserver User <u81451@betria.ispgateway.de>
2010-08-10 13:32:52 +0200

    Removed Debris from Homepage

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
   <link type="text/css" rel="stylesheet" media="all" href="/css/takeshi.css" />
   <script type="text/javascript" src="/js/functions.js"> </script>
 </head>
-<body  class="page-homepage sidebars-0 arg0-node" onload="loader();">	
+<body  class="page-homepage" onload="loader();">	
   
   <div id="header" class="region">
     <!-- start:header -->  
@@ -32,176 +32,122 @@
 
 
 
-<div class="node story published teaser story-teaser with-teaserimage age-0">
-
-    	    
+<div class="node">   	    
 		<h2 class="title"><a href="/2010/08/06/what-we-say-no-to.html">What we say no to</a></h2>
 		<p class="metadata">
 			<span class="date">6. 8. 2010<br/></span>		
 		</p>
 	
     <div class="content">
-
-      <p class='teaserimage'><img src='http://anmutunddemut.de/files/gettingrealreal.jpg' alt='What we say no to'/></p>    <p>Paul Graham – der Autor von <a href="http://www.paulgraham.com/hackpaint.html">Hackers and Painters</a> ist und dessen Webseite vielleicht nicht zufällig den Eindruck macht, als wäre sie statisches HTML – hat einen Artikel geschrieben mit dem Titel "<a href="http://www.paulgraham.com/addiction.html">The Acceleration of Addictiveness</a>", der mit diesem bemerkenswerte Zitat endet.</p>
-<blockquote><p>"We'll increasingly be defined by what we say no to."</p>
-</blockquote>
-   		
-	 	  <p class='readmore'><a href="/2010/08/06/what-we-say-no-to.html">Weiterlesen …</a></p>    </div>    
+        <p class='teaserimage'><img src='http://anmutunddemut.de/files/gettingrealreal.jpg' alt='What we say no to'/></p>    
+        <p>Paul Graham – der Autor von <a href="http://www.paulgraham.com/hackpaint.html">Hackers and Painters</a> ist und dessen Webseite vielleicht nicht zufällig den Eindruck macht, als wäre sie statisches HTML – hat einen Artikel geschrieben mit dem Titel "<a href="http://www.paulgraham.com/addiction.html">The Acceleration of Addictiveness</a>", der mit diesem bemerkenswerte Zitat endet.</p>
+        <blockquote><p>"We'll increasingly be defined by what we say no to."</p></blockquote>   		
+	 	    <p class='readmore'><a href="/2010/08/06/what-we-say-no-to.html">Weiterlesen …</a></p>    
+	 	</div>    
 </div>
 
-	
 
-
-
-<div class="node story published teaser story-teaser with-teaserimage age-0">
-    	    
+<div class="node">    	    
 		<h2 class="title"><a href="/2010/08/06/cast-away.html">Cast Away</a></h2>
 		<p class="metadata">
-			<span class="date">6. 8. 2010<br/></span>
-		
+			<span class="date">6. 8. 2010<br/></span>		
 		</p>
 	
     <div class="content">
-      <p class='teaserimage'><img src='http://anmutunddemut.de/files/castaway-asleep.jpg' alt='Cast Away'/></p>    <p>Die Qualität eines Filmes misst sich mitunter nicht am Unterhaltungswert während Du ihn schaust. Und mitunter auch nicht an den Gedanken und Eindrücken, aus denen Du nach dem Film Dein Urteil bildest. Mitunter misst sie sich auch nicht daran, wie perfekt alle seine Teile ein Ganzes ergeben, wie innovativ ein Film war, wie gut er altert und wieviele folgende Filme er beeinflusst hat. Sie bemisste sich mitunter nicht an irgendwelchen herausragenden Leistungen irgendwelcher Beteiligter.</p>
-   		
-	 	  <p class='readmore'><a href="/2010/08/06/cast-away.html">Weiterlesen …</a></p>    </div>    
+      <p class='teaserimage'><img src='http://anmutunddemut.de/files/castaway-asleep.jpg' alt='Cast Away'/></p>    
+      <p>Die Qualität eines Filmes misst sich mitunter nicht am Unterhaltungswert während Du ihn schaust. Und mitunter auch nicht an den Gedanken und Eindrücken, aus denen Du nach dem Film Dein Urteil bildest. Mitunter misst sie sich auch nicht daran, wie perfekt alle seine Teile ein Ganzes ergeben, wie innovativ ein Film war, wie gut er altert und wieviele folgende Filme er beeinflusst hat. Sie bemisste sich mitunter nicht an irgendwelchen herausragenden Leistungen irgendwelcher Beteiligter.</p>
+	 	  <p class='readmore'><a href="/2010/08/06/cast-away.html">Weiterlesen …</a></p>    
+	 	</div>    
 </div>
 
-	
 
-
-
-<div class="node story published teaser story-teaser with-teaserimage age-0">
-    	    
+<div class="node">    	    
 		<h2 class="title"><a href="/2010/08/05/das-ende-von-wave.html">Das Ende von Wave</a></h2>
-
 		<p class="metadata">
-			<span class="date">5. 8. 2010<br/></span>
-		
+			<span class="date">5. 8. 2010<br/></span>		
 		</p>
 	
     <div class="content">
-      <p class='teaserimage'><img src='http://anmutunddemut.de/files/waveamende.jpg' alt='Das Ende von Wave'/></p>    <p>Aus Chronistenpflich:<a href="http://googleblog.blogspot.com/2010/08/update-on-google-wave.html"> Google beendet Wave</a> zum Jahresende. Das kam jetzt überraschender als es hätte kommen sollen. Abgesehen von meiner Whisky-Wave und der "100 Dinge für ein besseres  Leben" Wave, die ich mit Dominik gemacht habe, trauere ich auch praktisch nichts hinterher, was dort passiert ist.</p>
-
-   		
-	 	      </div>    
+      <p class='teaserimage'><img src='http://anmutunddemut.de/files/waveamende.jpg' alt='Das Ende von Wave'/></p>    
+      <p>Aus Chronistenpflich:<a href="http://googleblog.blogspot.com/2010/08/update-on-google-wave.html"> Google beendet Wave</a> zum Jahresende. Das kam jetzt überraschender als es hätte kommen sollen. Abgesehen von meiner Whisky-Wave und der "100 Dinge für ein besseres  Leben" Wave, die ich mit Dominik gemacht habe, trauere ich auch praktisch nichts hinterher, was dort passiert ist.</p>
+    </div>    
 </div>
 
-	
 
-
-
-<div class="node story published teaser story-teaser with-teaserimage age-0">
-    	    
+<div class="node">    	    
 		<h2 class="title"><a href="/2010/08/04/never-underestimate-the-daylight.html">Never underestimate the daylight</a></h2>
 		<p class="metadata">
-			<span class="date">4. 8. 2010<br/></span>
-		
+			<span class="date">4. 8. 2010<br/></span>		
 		</p>
 	
     <div class="content">
       <p class='teaserimage'><img src='http://anmutunddemut.de/files/neverunderastimatethedaylight.jpg' alt='Never underestimate the daylight'/></p>       		
-	 	      </div>    
+	 	</div>    
 </div>
 
-	
 
-
-
-<div class="node story published teaser story-teaser with-teaserimage age-0">
-    	    
+<div class="node">    	    
 		<h2 class="title"><a href="/2010/08/04/virtuelles-privates-netzwerk.html">Virtuelles, privates Netzwerk</a></h2>
-
 		<p class="metadata">
-			<span class="date">4. 8. 2010<br/></span>
-			
+			<span class="date">4. 8. 2010<br/></span>			
 		</p>
 	
     <div class="content">
-      <p class='teaserimage'><img src='http://anmutunddemut.de/files/blackvpn.jpg' alt='Virtuelles, privates Netzwerk'/></p>    <p>Ich hab irgendwann hier schon mal geschrieben, dass ich mir wenige Sorgen um die Zukunft des freien Internets mache, weil die <i>guten</i> Jungs und Mädels, die bessere Software machen. 2006 hatte mir Programmiergott Sven schon <a href="http://anmutunddemut.de/2006/02/28/peer-to-peer-reborn-hamachi">Hamachi</a> als VPN-Client für Peer to Peer Späße angeraten. Nicht, dass ich den oft benutzt hätte.</p>
-
-   		
-	 	  <p class='readmore'><a href="/2010/08/04/virtuelles-privates-netzwerk.html">Weiterlesen …</a></p>    </div>    
+      <p class='teaserimage'><img src='http://anmutunddemut.de/files/blackvpn.jpg' alt='Virtuelles, privates Netzwerk'/></p>    
+      <p>Ich hab irgendwann hier schon mal geschrieben, dass ich mir wenige Sorgen um die Zukunft des freien Internets mache, weil die <i>guten</i> Jungs und Mädels, die bessere Software machen. 2006 hatte mir Programmiergott Sven schon <a href="http://anmutunddemut.de/2006/02/28/peer-to-peer-reborn-hamachi">Hamachi</a> als VPN-Client für Peer to Peer Späße angeraten. Nicht, dass ich den oft benutzt hätte.</p>
+	 	  <p class='readmore'><a href="/2010/08/04/virtuelles-privates-netzwerk.html">Weiterlesen …</a></p>    
+	 	</div>    
 </div>
 
-	
 
-
-
-<div class="node story published teaser story-teaser with-teaserimage age-0">
-    	    
+<div class="node">    	    
 		<h2 class="title"><a href="/2010/08/03/reductio-ad-naturum.html">Reductio ad Naturam</a></h2>
 		<p class="metadata">
-			<span class="date">3. 8. 2010<br/></span>
-			
+			<span class="date">3. 8. 2010<br/></span>			
 		</p>
 	
     <div class="content">
-      <p class='teaserimage'><img src='http://anmutunddemut.de/files/reductionadnaturam.jpg' alt='Reductio ad Naturam'/></p>    <p>Ein weiteres <a href="http://anmutunddemut.de/lexikon/scheinargument.html">Scheinargument</a>, dass ich nicht mehr gelten lasse, bezeichne ich als Reduction ad Naturam. Es bezeichnet jene Argumente, die einen wie auch immer gearteten vorhistorische Naturzustand der menschlichen Entwicklung heranziehen, um irgendwelche bewußten Verhaltensweisen lebender Menschen zu erklären. So zum Beispiel die Idee, dass das stete Reagieren auf Blinken und Piepsen von Gagdets sich davon ableiten ließe, dass man ja auf wilde Tiere, die im Gebüsch lauerten, als Urmensch auch schnell reagieren müsse.</p>
-   		
-	 	  <p class='readmore'><a href="/2010/08/03/reductio-ad-naturum.html">Weiterlesen …</a></p>    </div>    
-
+      <p class='teaserimage'><img src='http://anmutunddemut.de/files/reductionadnaturam.jpg' alt='Reductio ad Naturam'/></p>    
+      <p>Ein weiteres <a href="http://anmutunddemut.de/lexikon/scheinargument.html">Scheinargument</a>, dass ich nicht mehr gelten lasse, bezeichne ich als Reduction ad Naturam. Es bezeichnet jene Argumente, die einen wie auch immer gearteten vorhistorische Naturzustand der menschlichen Entwicklung heranziehen, um irgendwelche bewußten Verhaltensweisen lebender Menschen zu erklären. So zum Beispiel die Idee, dass das stete Reagieren auf Blinken und Piepsen von Gagdets sich davon ableiten ließe, dass man ja auf wilde Tiere, die im Gebüsch lauerten, als Urmensch auch schnell reagieren müsse.</p>
+	 	  <p class='readmore'><a href="/2010/08/03/reductio-ad-naturum.html">Weiterlesen …</a></p>    
+	 	</div>   
 </div>
 
-	
 
-
-
-<div class="node story published teaser story-teaser with-teaserimage age-0">
-    	    
+<div class="node">    	    
 		<h2 class="title"><a href="/2010/08/03/das-schicksal-von-nazis-ist-mir-komplet">Das Schicksal von Nazis ist mir komplett gleichgültig</a></h2>
 		<p class="metadata">
 			<span class="date">3. 8. 2010<br/></span>
-			
-
 		</p>
 	
     <div class="content">
       <p class='teaserimage'><img src='http://anmutunddemut.de/files/dasschicksalvonnazis.jpg' alt='Das Schicksal von Nazis ist mir komplett gleichgültig'/></p>    <blockquote><p>"Das Schicksal von Nazis ist mir komplett gleichgültig; ob sie hungern, frieren, bettnässen, schlecht träumen usw. geht mich nichts an. Was mich an ihnen interessiert, ist nur eins: daß man sie hindert, das zu tun, was sie eben tun, wenn man sie nicht hindert: die bedrohen und nach Möglichkeit umbringen, die nicht in ihre Zigarettenschachtelwelt passen. Ob man sie dafür einsperrt oder sie dafür auf den Obduktionstisch gelegt werden müssen, ist mir gleich, und wer vom Lager (für andere) träumt, kann gerne selbst hinein."</p></blockquote>
-<p>Wiglaf Droste</p>
-   		
-	 	      </div>    
+      <p>Wiglaf Droste</p>
+   	</div>    
 </div>
 
-	
 
-
-
-<div class="node moblog published teaser moblog-teaser age-0">
-    	    
+<div class="node">    	    
 		<h2 class="title"><a href="/2010/08/03/hamburg.html">Hamburg</a></h2>
-
 		<p class="metadata">
-			<span class="date">3. 8. 2010<br/></span>
-			
+			<span class="date">3. 8. 2010<br/></span>			
 		</p>
 	
     <div class="content">
           <img src='http://anmutunddemut.de/files/moblog-c901/DSC04093.JPG' alt='Hamburg' />
-
-   		
-	 	      </div>    
+    </div>    
 </div>
 
-	
-
-
-
-<div class="node story published teaser story-teaser with-teaserimage age-0">
-    	    
+<div class="node">    	    
 		<h2 class="title"><a href="/2010/08/02/richardkiez-mein-kiez.html">Richardkiez – mein Kiez</a></h2>
 		<p class="metadata">
-			<span class="date">2. 8. 2010<br/></span>
-			<span class="commentcount"><a href="/2010/08/02/richardkiez-mein-kiez.html#comments">11 Kommentare</a><br/></span>
-
-			<span class="clickcount">178 mal gelesen</span>
+			<span class="date">2. 8. 2010<br/></span>			
 		</p>
 	
     <div class="content">
       <p class='teaserimage'><img src='http://anmutunddemut.de/files/richardkiez.jpg' alt='Richardkiez – mein Kiez'/></p>    <p><i>"Dit wollt ick aber jerade sachn, da könnse schomma nen Türkischkurs belejen, wennse dahinziehn."</i> hat der Taxifahrer zu mir gesagt, als ich ihm erzählt habe, dass ich gerade glücklich den Mietvertrag ne Wohung am <a href="http://de.wikipedia.org/wiki/Richardplatz">Richardplatz</a> in <a href="http://de.wikipedia.org/wiki/Berlin-Neuk%C3%B6lln">Neukölln</a> unterschrieben hätte. Irgendwie hatte er ein bisken Sorge, dass ich keine Ahnung habe, wo ich da eigentlich hinziehe. Und das kann ich sogar verstehen.</p>
-
-   		
-	 	  <p class='readmore'><a href="/2010/08/02/richardkiez-mein-kiez.html">Weiterlesen …</a></p>    </div>    
+	 	  <p class='readmore'><a href="/2010/08/02/richardkiez-mein-kiez.html">Weiterlesen …</a></p>    
+	 	</div>    
 </div>
 
   
