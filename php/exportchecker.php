13d088e5381cb0420ee2ef8f29893a17c1152a9a
Ben <benjamin@birkenhake.org>
2010-08-13 13:08:36 +0200

    Links and filenames fixed

--- php/exportchecker.php
+++ php/exportchecker.php
@@ -1,15 +1,35 @@
+
 <?
 
 $file = file("drupal_url_alias.csv");
 $counter = 0;
+$aliase = array();
 foreach($file as $line){
   $array = explode(";", $line);
   //print $array[1];
+  $aliase[] = $array[0];
   $name = "../".trim($array[1]).".html";
   if(!file_exists($name)){
-    print "ERROR: ".$name."\n";
-    die;
-    $counter ++;
+    //print "ERROR: ".$name."\n";
+    //die;
+    
   }
 }
+
+//print_r($aliase);
+
+$file2 = file("drupal_node.csv");
+
+foreach($file2 as $line){
+  $array = array();
+  $array = explode(";", $line);
+  if(!in_array("node/".trim($array[0]), $aliase)){
+    print $array[0]." - ".$array[1]."\n";
+    $counter ++;
+  }
+  
+}
+
+
+
 print $counter." Errors\n";
