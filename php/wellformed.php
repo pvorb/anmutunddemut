d0de57d09566811cd2f39210d1a897351fd2e895
Ben <benjamin@birkenhake.org>
2010-08-25 17:51:16 +0200

    Repaired an Article

--- php/wellformed.php
+++ php/wellformed.php
@@ -3,7 +3,10 @@
 include("heimweh.php");
 $docs = directory_to_array_recursive("..");
 foreach($docs as $doc){    
-  if(substr($doc, -4, 4)=="html"){
+  if(substr($doc, -4, 4)=="html" 
+  and !($doc=="../php/directory-listing-header.html" or $doc=="../php/directory-listing-footer.html")
+  and (substr($doc, "0", "11")!= "../projekte")
+  ){
     $xml = simplexml_load_file($doc);
     if(get_class($xml)!="SimpleXMLElement"){
       print $doc." is not wellformed \n";
